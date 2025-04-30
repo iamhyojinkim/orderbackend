424734eb3c46ccf28ce9598fa885d54da816d603
hyojin kim <h20163820@gmail.com>
2025-04-30 09:18:40 +0900

    변경 사항에 대한 커밋 메시지

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
@@ -6,13 +7,20 @@ const indexRouter = require("./routes/index");
 require("dotenv").config();
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
-
 app.use(bodyParser.json());
 app.use("/api", indexRouter);
+
 const mongoURI = process.env.MONGODB_URI_PROD;
 
-mongoose.connect(mongoURI).then(() => console.log("mongoose connected"));
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log("mongoose connected", mongoURI))
+  .catch((err) => console.log("MongoDB 연결 실패:", err));
 
-app.listen(3000, () => console.log("server on 3000"));
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`서버가 ${port}번 포트에서 실행 중`);
+});
